781967c99abd01d89bb0008d9761ddeacc617792
ramith123 <ramithchandula@gmail.com>
2020-11-03 14:34:33 -0400

    format fixes

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import unittest
 import calculator
 
+
 class Tests(unittest.TestCase):
     def test_add(self):
         self.assertEqual(calculator.add(2, 3), 5)
-    
+
     def test_sub(self):
         self.assertEqual(calculator.sub(2, 3), -1)
 
@@ -15,5 +16,5 @@ class Tests(unittest.TestCase):
         self.assertEqual(calculator.div(6, 3), 2)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
